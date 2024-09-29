b70e35adc7bdc6737c752affb28137324ab4aa74
DDunis <unknown901115@gmail.com>
2024-09-29 15:08:40 +0900

    Initial Commit

---
+++ Apple_Framword/Apple_FramwordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FramwordApp.swift
+//  Apple_Framword
+//
+//  Created by J on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FramwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple_FramwordTests/Apple_FramwordTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FramwordTests.swift
+//  Apple_FramwordTests
+//
+//  Created by J on 9/29/24.
+//
+
+import Testing
+@testable import Apple_Framword
+
+struct Apple_FramwordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
